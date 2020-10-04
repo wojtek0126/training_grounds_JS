9d1bc5e353a3c5ae1335f4ea5698ff4f0f062c09
wojtek0126 <wojtek0126@gmail.com>
2020-10-04 12:27:21 +0200

    cacoguchi JS

--- js/app.js
+++ js/app.js
@@ -108,9 +108,9 @@ const  fire = getElement('.fire');
 const dump = getElement('.dump');
 const dot = getElement('.dot');
 const caco = document.getElementById('caco1');
-down.addEventListener('click', function () {
-    caco.style.transform = 'translate(0, 200px)';
-})
+// down.addEventListener('click', function () {
+//     caco.style.transform = 'translate(0, 200px)';
+// })
 
 up.addEventListener('click', function () {
     caco.style.transform = 'translate(0, -0px)';
@@ -119,10 +119,13 @@ up.addEventListener('click', function () {
 left.addEventListener('click', function () {
     caco.style.transform = 'translate(200px, 0px)';
     caco.classList.add('caco-turn--anim');
-    if(caco.classList.contains('caco')) {
-        caco.classList.remove('caco');
+    if(caco.classList.contains('caco-left')) {
+        caco.classList.remove('caco-left');
         caco.classList.add('caco-right');
-
+    }
+    else if(caco.classList.contains('caco-down')) {
+        caco.classList.remove('caco-down');
+        caco.classList.add('caco-right');
     }
     setTimeout(function () {
         caco.classList.remove('caco-turn--anim');
@@ -141,8 +144,31 @@ right.addEventListener('click', function () {
     caco.classList.add('caco-turn--anim');
     if(caco.classList.contains('caco-right')) {
         caco.classList.remove('caco-right');
-        caco.classList.add('caco');
+        caco.classList.add('caco-left');
+    }
+    else if(caco.classList.contains('caco-down')) {
+        caco.classList.remove('caco-down');
+        caco.classList.add('caco-left');
+    }
+    setTimeout(function () {
+        caco.classList.remove('caco-turn--anim');
+        caco.classList.add('caco-return--anim');
+    }, 1000)
+    caco.classList.remove('caco-return--anim');
+    setTimeout(function () {
+        caco.classList.remove('caco-return--anim');}, 200)
+})
 
+down.addEventListener('click', function () {
+    caco.style.transform = 'translate(0, 200px)';
+    caco.classList.add('caco-turn--anim');
+    if(caco.classList.contains('caco-right')) {
+        caco.classList.remove('caco-right');
+        caco.classList.add('caco-down');
+    }
+    else if(caco.classList.contains('caco-left')) {
+        caco.classList.remove('caco-left');
+        caco.classList.add('caco-down');
     }
     setTimeout(function () {
         caco.classList.remove('caco-turn--anim');
